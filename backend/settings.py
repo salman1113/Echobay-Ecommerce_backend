d2b7fe2e50c71ab816d33b2d78e7bffb684a021a
salman <mrsalmanxzs@gmail.com>
2025-12-24 12:08:25 +0530

    Fixed CORS

--- backend/settings.py
+++ backend/settings.py
@@ -187,3 +187,4 @@ RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID')
 RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #Hosting
+CORS_ALLOW_ALL_ORIGINS = True #Hosting
