8abf2cbb0e7d0b96a771b00158a6f542260de914
salman <mrsalmanxzs@gmail.com>
2025-12-24 14:22:29 +0530

    Fixed

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
